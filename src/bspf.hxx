969ded6af529c95c54e9a770003f3226cff849ca
Renato L. F. Cunha <renatocunha@acm.org>
2017-07-29 09:34:19 -0300

    add support files for color averaging

---
+++ src/bspf.hxx
@@ -0,0 +1,11 @@
+// Types for 8-bit signed and unsigned integers
+typedef signed char Int8;
+typedef unsigned char uInt8;
+
+// Types for 16-bit signed and unsigned integers
+typedef signed short Int16;
+typedef unsigned short uInt16;
+
+// Types for 32-bit signed and unsigned integers
+typedef signed int Int32;
+typedef unsigned int uInt32;
